1b60a1ed007b3cc52c8f2ec36e3e28ae63000beb
Miz <Miz@DESKTOP-SG1DAI4>
2019-04-06 02:38:36 -0300

    Atualização 06/04/2019

--- 02 - Estrutura de Decisão_Aula 14-02/Lt01_EstDec30.java
+++ 02 - Estrutura de Decisão_Aula 14-02/Lt01_EstDec30.java
@@ -1,8 +1,8 @@
 /**
- * Objetivo:        Receba a data de nascimento e atual em ano, mês e dia. Calcule e mostre a 
+ * Objetivo:        Receba a data de nascimento e atual em ano, mï¿½s e dia. Calcule e mostre a 
  * 					idade em anos, meses e dias, considerando os anos bissextos.
  * Programadora:    Kesia Amanda
- * Data:            16/02/2019
+ * Data:            06/04/2019
  */
 
 import javax.swing.JOptionPane;
@@ -12,22 +12,47 @@ public class Lt01_EstDec30
 	public static void main (String[] args)
 	{		
 		int diaAtual = Integer.parseInt(JOptionPane.showInputDialog("Informe o dia atual: "));
-		int mesAtual = Integer.parseInt(JOptionPane.showInputDialog("Informe o mês atual: "));
+		int mesAtual = Integer.parseInt(JOptionPane.showInputDialog("Informe o mï¿½s atual: "));
 		int anoAtual = Integer.parseInt(JOptionPane.showInputDialog("Informe o ano atual: "));
 		int diaNasc = Integer.parseInt(JOptionPane.showInputDialog("Informe o seu dia de nascimento: "));
-		int mesNasc = Integer.parseInt(JOptionPane.showInputDialog("Informe o seu mês de nascimento: "));
+		int mesNasc = Integer.parseInt(JOptionPane.showInputDialog("Informe o seu mï¿½s de nascimento: "));
 		int anoNasc = Integer.parseInt(JOptionPane.showInputDialog("Informe o seu ano de nascimento: "));
 		
 		int difDia = diaAtual - diaNasc;
 		int difMes = mesAtual - mesNasc;
 		int difAno = anoAtual - anoNasc;
-		
-		int mesPassado = 0;
-		
-		if (difDia<0)
-		{
 			
+		if (difDia<0){
+			difMes-=1;
+			difDia+=FuncDiasMesPassado(mesAtual,anoAtual);
 		}
-		
+		if (difMes<0){
+			difMes=12+difMes;
+			difAno-=1;	
+		}
+				
+		System.out.print("Anos: "+difAno+" meses: "+difMes+" dias: "+difDia);
+	}
+	
+	static int FuncDiasMesPassado (int mesAtual, int anoAtual) {
+		if (mesAtual==3) {
+			return FunBissexto(anoAtual);
+		}else if (mesAtual==5||mesAtual==7||mesAtual==10||mesAtual==12) {
+			return 30;
+		}else {
+			return 31;
+		}
+	}
+	
+	static int FunBissexto (int anoAtual) {
+		if (anoAtual%4==0) {
+			if (anoAtual%100==0 && anoAtual%400!=0) {
+				return 28;
+			}else {
+				return 29;
+			}
+		}else {
+			return 28;
+		}
 	}
 }
